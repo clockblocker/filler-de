3170e423c79da69483e82fc25ee7bb09379ca2ae
clockblocker <asde3211q@gmail.com>
2025-09-09 06:39:37 +0200

    css codegen wip

---
+++ .src/codegen/generated/block-classes.ts
@@ -0,0 +1,8 @@
+// AUTO-GENERATED FILE. Do not edit by hand.
+export const NOTE_BLOCK_CLASS = 'note_block';
+export const NOTE_BLOCK_PREFIX = 'note_block_';
+
+export const BLOCK_CLASSES = {
+	actions: `${NOTE_BLOCK_CLASS} ${NOTE_BLOCK_PREFIX}actions`,
+	morphems: `${NOTE_BLOCK_CLASS} ${NOTE_BLOCK_PREFIX}morphems`,
+} as const;
