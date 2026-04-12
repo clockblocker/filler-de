926827a91844317d71a335cbcef872ad409de58e
clockblocker <asde3211q@gmail.com>
2026-04-12 10:33:13 +0200

    fix ts errors

--- src/packages/independent/linguistics/src/german/lu/lexeme/shared/build-german-lexeme-bundle.ts
+++ src/packages/independent/linguistics/src/german/lu/lexeme/shared/build-german-lexeme-bundle.ts
@@ -5,6 +5,34 @@ import type { Pos } from "../../../../universal/enums/kind/pos";
 import { buildInflectionSelection } from "../../../../universal/factories/buildInflectionSelection";
 import { buildLemmaSelection } from "../../../../universal/factories/buildLemmaSelection";
 
+type GermanLexemeBundle<P extends Pos> = {
+	InflectionSelectionSchema: z.ZodType<
+		AbstractSelectionFor<"Standard", "Inflection", "Lexeme", P>
+	>;
+	LemmaSchema: z.ZodType<AbstractLemma<"Lexeme", P>>;
+	LemmaSelectionSchema: z.ZodType<
+		AbstractSelectionFor<"Standard", "Lemma", "Lexeme", P>
+	>;
+	StandardPartialSelectionSchema: z.ZodType<
+		AbstractSelectionFor<"Standard", "Partial", "Lexeme", P>
+	>;
+	StandardVariantSelectionSchema: z.ZodType<
+		AbstractSelectionFor<"Standard", "Variant", "Lexeme", P>
+	>;
+	TypoInflectionSelectionSchema: z.ZodType<
+		AbstractSelectionFor<"Typo", "Inflection", "Lexeme", P>
+	>;
+	TypoLemmaSelectionSchema: z.ZodType<
+		AbstractSelectionFor<"Typo", "Lemma", "Lexeme", P>
+	>;
+	TypoPartialSelectionSchema: z.ZodType<
+		AbstractSelectionFor<"Typo", "Partial", "Lexeme", P>
+	>;
+	TypoVariantSelectionSchema: z.ZodType<
+		AbstractSelectionFor<"Typo", "Variant", "Lexeme", P>
+	>;
+};
+
 export function buildGermanLexemeBundle<
 	P extends Pos,
 	InflectionalFeaturesSchema extends z.ZodTypeAny,
@@ -23,7 +51,7 @@ export function buildGermanLexemeBundle<
 	lexicalRelationsSchema: LexicalRelationsSchema;
 	morphologicalRelationsSchema: MorphologicalRelationsSchema;
 	pos: P;
-}) {
+}): GermanLexemeBundle<P> {
 	const lemmaIdentityShape = {
 		lemmaKind: z.literal("Lexeme"),
 		pos: z.literal(pos),
@@ -33,58 +61,42 @@ export function buildGermanLexemeBundle<
 		InflectionSelectionSchema: buildInflectionSelection({
 			inflectionalFeaturesSchema,
 			lemmaIdentityShape,
-		}) satisfies z.ZodType<
-			AbstractSelectionFor<"Standard", "Inflection", "Lexeme", P>
-		>,
+		}) as unknown as GermanLexemeBundle<P>["InflectionSelectionSchema"],
 		LemmaSchema: z.object({
 			inherentFeatures: inherentFeaturesSchema,
 			lexicalRelations: lexicalRelationsSchema,
 			morphologicalRelations: morphologicalRelationsSchema,
 			pos: z.literal(pos),
-		}) satisfies z.ZodType<AbstractLemma<"Lexeme", P>>,
+		}) as unknown as GermanLexemeBundle<P>["LemmaSchema"],
 		LemmaSelectionSchema: buildLemmaSelection({
 			lemmaIdentityShape,
-		}) satisfies z.ZodType<
-			AbstractSelectionFor<"Standard", "Lemma", "Lexeme", P>
-		>,
+		}) as unknown as GermanLexemeBundle<P>["LemmaSelectionSchema"],
 		StandardPartialSelectionSchema: buildLemmaSelection({
 			lemmaIdentityShape,
 			surfaceKind: "Partial",
-		}) satisfies z.ZodType<
-			AbstractSelectionFor<"Standard", "Partial", "Lexeme", P>
-		>,
+		}) as unknown as GermanLexemeBundle<P>["StandardPartialSelectionSchema"],
 		StandardVariantSelectionSchema: buildLemmaSelection({
 			lemmaIdentityShape,
 			surfaceKind: "Variant",
-		}) satisfies z.ZodType<
-			AbstractSelectionFor<"Standard", "Variant", "Lexeme", P>
-		>,
+		}) as unknown as GermanLexemeBundle<P>["StandardVariantSelectionSchema"],
 		TypoInflectionSelectionSchema: buildInflectionSelection({
 			inflectionalFeaturesSchema,
 			lemmaIdentityShape,
 			orthographicStatus: "Typo",
-		}) satisfies z.ZodType<
-			AbstractSelectionFor<"Typo", "Inflection", "Lexeme", P>
-		>,
+		}) as unknown as GermanLexemeBundle<P>["TypoInflectionSelectionSchema"],
 		TypoLemmaSelectionSchema: buildLemmaSelection({
 			lemmaIdentityShape,
 			orthographicStatus: "Typo",
-		}) satisfies z.ZodType<
-			AbstractSelectionFor<"Typo", "Lemma", "Lexeme", P>
-		>,
+		}) as unknown as GermanLexemeBundle<P>["TypoLemmaSelectionSchema"],
 		TypoPartialSelectionSchema: buildLemmaSelection({
 			lemmaIdentityShape,
 			orthographicStatus: "Typo",
 			surfaceKind: "Partial",
-		}) satisfies z.ZodType<
-			AbstractSelectionFor<"Typo", "Partial", "Lexeme", P>
-		>,
+		}) as unknown as GermanLexemeBundle<P>["TypoPartialSelectionSchema"],
 		TypoVariantSelectionSchema: buildLemmaSelection({
 			lemmaIdentityShape,
 			orthographicStatus: "Typo",
 			surfaceKind: "Variant",
-		}) satisfies z.ZodType<
-			AbstractSelectionFor<"Typo", "Variant", "Lexeme", P>
-		>,
+		}) as unknown as GermanLexemeBundle<P>["TypoVariantSelectionSchema"],
 	};
 }
