2544f2513fc6130a12ca3ac08674d5e9b8737b44
clockblocker <asde3211q@gmail.com>
2026-04-12 11:02:37 +0200

    move lexemes in sep file

--- src/packages/independent/linguistics/src/german/german-lemma.ts
+++ src/packages/independent/linguistics/src/german/german-lemma.ts
@@ -1,43 +1,8 @@
 import z from "zod/v3";
 import type { LemmaSchemaLanguageShape } from "../registry-shapes";
-import { GermanAdjectiveLemmaSchema } from "./lu/lexeme/adjective/german-adjective-bundle";
-import { GermanAdpositionLemmaSchema } from "./lu/lexeme/adposition/german-adposition-bundle";
-import { GermanAdverbLemmaSchema } from "./lu/lexeme/adverb/german-adverb-bundle";
-import { GermanAuxiliaryLemmaSchema } from "./lu/lexeme/auxiliary/german-auxiliary-bundle";
-import { GermanCoordinatingConjunctionLemmaSchema } from "./lu/lexeme/coordinating-conjunction/german-coordinating-conjunction-bundle";
-import { GermanDeterminerLemmaSchema } from "./lu/lexeme/determiner/german-determiner-bundle";
-import { GermanInterjectionLemmaSchema } from "./lu/lexeme/interjection/german-interjection-bundle";
-import { GermanNounLemmaSchema } from "./lu/lexeme/noun/german-noun-bundle";
-import { GermanNumeralLemmaSchema } from "./lu/lexeme/numeral/german-numeral-bundle";
-import { GermanOtherLemmaSchema } from "./lu/lexeme/other/german-other-bundle";
-import { GermanParticleLemmaSchema } from "./lu/lexeme/particle/german-particle-bundle";
-import { GermanPronounLemmaSchema } from "./lu/lexeme/pronoun/german-pronoun-bundle";
-import { GermanProperNounLemmaSchema } from "./lu/lexeme/proper-noun/german-proper-noun-bundle";
-import { GermanPunctuationLemmaSchema } from "./lu/lexeme/punctuation/german-punctuation-bundle";
-import { GermanSubordinatingConjunctionLemmaSchema } from "./lu/lexeme/subordinating-conjunction/german-subordinating-conjunction-bundle";
-import { GermanSymbolLemmaSchema } from "./lu/lexeme/symbol/german-symbol-bundle";
-import { GermanVerbLemmaSchema } from "./lu/lexeme/verb/german-verb-bundle";
+import { GermanLexemeLemmaSchemas } from "./lu/lexeme/german-lexemes";
 
 const AnySchema = z.any();
-const GermanLexemeLemmaSchemas = {
-	ADJ: GermanAdjectiveLemmaSchema,
-	ADP: GermanAdpositionLemmaSchema,
-	ADV: GermanAdverbLemmaSchema,
-	AUX: GermanAuxiliaryLemmaSchema,
-	CCONJ: GermanCoordinatingConjunctionLemmaSchema,
-	DET: GermanDeterminerLemmaSchema,
-	INTJ: GermanInterjectionLemmaSchema,
-	NOUN: GermanNounLemmaSchema,
-	NUM: GermanNumeralLemmaSchema,
-	PART: GermanParticleLemmaSchema,
-	PRON: GermanPronounLemmaSchema,
-	PROPN: GermanProperNounLemmaSchema,
-	PUNCT: GermanPunctuationLemmaSchema,
-	SCONJ: GermanSubordinatingConjunctionLemmaSchema,
-	SYM: GermanSymbolLemmaSchema,
-	VERB: GermanVerbLemmaSchema,
-	X: GermanOtherLemmaSchema,
-} satisfies LemmaSchemaLanguageShape["Lexeme"];
 
 export const GermanLemmaSchema = {
 	Lexeme: GermanLexemeLemmaSchemas,
