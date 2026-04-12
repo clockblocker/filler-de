b9489b9871f921223024f01f032878ff89088029
clockblocker <asde3211q@gmail.com>
2026-04-12 09:38:52 +0200

    remove bait

--- src/packages/independent/linguistics/src/german/german-lemma.ts
+++ src/packages/independent/linguistics/src/german/german-lemma.ts
@@ -1,28 +1,27 @@
 import z from "zod/v3";
 import type { LemmaSchemaLanguageShape } from "../registry-shapes";
-import { GermanLexemePosSchemas } from "./lu/lexeme/pos/german-pos-schemas";
 
 const AnySchema = z.any();
 
 export const GermanLemmaSchema = {
 	Lexeme: {
-		ADJ: GermanLexemePosSchemas.ADJ.lemmaSchema,
-		ADP: GermanLexemePosSchemas.ADP.lemmaSchema,
-		ADV: GermanLexemePosSchemas.ADV.lemmaSchema,
-		AUX: GermanLexemePosSchemas.AUX.lemmaSchema,
-		CCONJ: GermanLexemePosSchemas.CCONJ.lemmaSchema,
-		DET: GermanLexemePosSchemas.DET.lemmaSchema,
-		INTJ: GermanLexemePosSchemas.INTJ.lemmaSchema,
-		NOUN: GermanLexemePosSchemas.NOUN.lemmaSchema,
-		NUM: GermanLexemePosSchemas.NUM.lemmaSchema,
-		PART: GermanLexemePosSchemas.PART.lemmaSchema,
-		PRON: GermanLexemePosSchemas.PRON.lemmaSchema,
-		PROPN: GermanLexemePosSchemas.PROPN.lemmaSchema,
-		PUNCT: GermanLexemePosSchemas.PUNCT.lemmaSchema,
-		SCONJ: GermanLexemePosSchemas.SCONJ.lemmaSchema,
-		SYM: GermanLexemePosSchemas.SYM.lemmaSchema,
-		VERB: GermanLexemePosSchemas.VERB.lemmaSchema,
-		X: GermanLexemePosSchemas.X.lemmaSchema,
+		ADJ: AnySchema,
+		ADP: AnySchema,
+		ADV: AnySchema,
+		AUX: AnySchema,
+		CCONJ: AnySchema,
+		DET: AnySchema,
+		INTJ: AnySchema,
+		NOUN: AnySchema,
+		NUM: AnySchema,
+		PART: AnySchema,
+		PRON: AnySchema,
+		PROPN: AnySchema,
+		PUNCT: AnySchema,
+		SCONJ: AnySchema,
+		SYM: AnySchema,
+		VERB: AnySchema,
+		X: AnySchema,
 	},
 	Morpheme: {
 		Circumfix: AnySchema,
